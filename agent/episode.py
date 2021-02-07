9b0f6a78205e3633b2f4b1ca53c40d5a04826a3c
Robin Carter <68642507+RobinLeeCarter@users.noreply.github.com>
2021-02-07 14:53:49 +0000

    Enable nice access of episode by time-step

--- agent/episode.py
+++ agent/episode.py
@@ -19,6 +19,9 @@ class Episode:
         # S0, A0, R1, S1, A1, R2 ... S(T-1), A(T-1), R(T)
         self.trajectory.append(rsa_)
 
+    def __getitem__(self, t: int) -> rsa.RSA:
+        return self.trajectory[t]
+
     # @property
     # def prev_rsa(self) -> rsa.RSA:
     #     return self.trajectory[-2]
