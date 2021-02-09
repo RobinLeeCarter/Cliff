54b75c18716889206ae9d09e631976d70e2eaf6b
Robin Carter <68642507+RobinLeeCarter@users.noreply.github.com>
2021-02-09 10:56:21 +0000

    Refined test in project_imports.py and added to utils as a standalone

---
+++ utils/project_imports.py
@@ -0,0 +1,28 @@
+# runs standalone, don't import this file
+from __future__ import annotations
+
+import sys
+import importlib
+
+
+def project_dependencies(module_name: str):
+    importlib.import_module(module_name)
+
+    app_dir = '/home/robin/Python/Projects/'
+
+    imported_module_names = []
+
+    for mod_name, mod in sys.modules.items():
+        file = getattr(mod, '__file__', '')
+        if str(file).startswith(app_dir)\
+                and mod_name != '__main__'\
+                and mod_name != module_name\
+                and not mod_name.startswith(module_name + "."):
+            imported_module_names.append(mod_name)
+
+    for mod_name in sorted(imported_module_names):
+        print(mod_name)
+
+
+if __name__ == '__main__':
+    project_dependencies('agent')
