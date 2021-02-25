bea6e4932eb8bc22a858a393c273e79c94a7dad4
Robin Carter <68642507+RobinLeeCarter@users.noreply.github.com>
2021-02-25 20:33:49 +0000

    start random walk comparison settings NoPolicy is still returning stationary move

--- mdp/model/algorithm/factory.py
+++ mdp/model/algorithm/factory.py
@@ -4,7 +4,7 @@ from typing import TYPE_CHECKING, Type
 if TYPE_CHECKING:
     from mdp.model import environment, agent
 from mdp import common
-from mdp.model.algorithm import abstract, control
+from mdp.model.algorithm import abstract, control, policy_evaluation
 
 
 def factory(environment_: environment.Environment,
@@ -15,7 +15,9 @@ def factory(environment_: environment.Environment,
         a.EXPECTED_SARSA: control.ExpectedSarsa,
         a.Q_LEARNING: control.QLearning,
         a.SARSA: control.Sarsa,
-        a.VQ: control.VQ
+        a.VQ: control.VQ,
+        a.CONSTANT_ALPHA_MC: policy_evaluation.ConstantAlphaMC,
+        a.TD_0: policy_evaluation.TD0,
     }
     type_for_algorithm = algorithm_lookup[algorithm_parameters.algorithm_type]
     algorithm_ = type_for_algorithm(environment_, agent_, algorithm_parameters)
---
+++ mdp/model/scenarios/random_walk/comparisons.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+from mdp import common
+
+
+def alpha() -> common.Comparison:
+    comparison = common.Comparison(
+        environment_parameters=common.EnvironmentParameters(
+            environment_type=common.EnvironmentType.RANDOM_WALK,
+        ),
+        comparison_settings=common.Settings(
+            runs=10,
+            training_episodes=100,
+            policy_parameters=common.PolicyParameters(
+                policy_type=common.PolicyType.NONE
+            )
+        ),
+        breakdown_parameters=common.BreakdownParameters(
+            breakdown_type=common.BreakdownType.RETURN_BY_EPISODE,
+        ),
+        settings_list=[
+            common.Settings(
+                algorithm_parameters=common.AlgorithmParameters(
+                    algorithm_type=common.AlgorithmType.TD_0,
+                    alpha=0.5
+                )
+            ),
+        ],
+    )
+    return comparison
