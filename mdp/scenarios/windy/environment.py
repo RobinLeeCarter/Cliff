b8e77b8fec63c663b210ddc681aaeffbd9cc9a01
Robin Carter <68642507+RobinLeeCarter@users.noreply.github.com>
2021-03-23 18:30:03 +0000

    Dynamics implemented for Windy

--- mdp/scenarios/windy/environment.py
+++ mdp/scenarios/windy/environment.py
@@ -4,19 +4,14 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from mdp import common
-from mdp.model.environment import Response
 from mdp.scenarios.position_move.model import environment
 
 from mdp.scenarios.windy.grid_world import GridWorld
+from mdp.scenarios.windy.dynamics import Dynamics
 
 
 class Environment(environment.Environment):
     def __init__(self, environment_parameters: common.EnvironmentParameters):
         super().__init__(environment_parameters)
         self.grid_world: GridWorld = GridWorld(environment_parameters)
-
-    def _get_response(self) -> Response:
-        return Response(
-            reward=-1.0,
-            state=self._new_state
-        )
+        self.dynamics = Dynamics(environment_=self, environment_parameters=self._environment_parameters)
