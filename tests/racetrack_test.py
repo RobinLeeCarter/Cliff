6ee8050eed2795ad332863f4ca89886c8bf2de54
Robin Carter <68642507+RobinLeeCarter@users.noreply.github.com>
2021-04-01 21:33:58 +0100

    WIP: tests fixed and green

--- tests/racetrack_test.py
+++ tests/racetrack_test.py
@@ -1,83 +1,46 @@
 from __future__ import annotations
 
 from mdp import common
-from unused import unused_environment_factory
-from mdp.scenarios.racetrack.scenario import comparison
-from mdp.scenarios.racetrack.model import action, grids, state, environment_parameters
+from mdp.scenarios.racetrack.model import action, grids, state
+from mdp.scenarios.racetrack.model.environment import Environment
+from mdp.scenarios.racetrack.model.environment_parameters import EnvironmentParameters, default
 
 
 def racetrack_test() -> bool:
-    comparison_ = comparison.Comparison(
-        environment_parameters=environment_parameters.EnvironmentParameters(
-            grid=grids.TRACK_1
-        ),
-        comparison_settings=common.Settings(
-            runs=1,
-            training_episodes=10_000,
-            # display_every_step=True,
-            dual_policy_relationship=common.DualPolicyRelationship.LINKED_POLICIES
-        ),
-        breakdown_parameters=common.BreakdownParameters(
-            breakdown_type=common.BreakdownType.RETURN_BY_EPISODE
-        ),
-        settings_list=[
-            # common.Settings(algorithm_parameters=common.AlgorithmParameters(
-            #     algorithm_type=common.AlgorithmType.EXPECTED_SARSA,
-            #     alpha=0.9
-            # )),
-            # common.Settings(algorithm_parameters=common.AlgorithmParameters(
-            #     algorithm_type=common.AlgorithmType.VQ,
-            #     alpha=0.2
-            # )),
-            # common.Settings(algorithm_parameters=common.AlgorithmParameters(
-            #     algorithm_type=common.AlgorithmType.Q_LEARNING,
-            #     alpha=0.5
-            # )),
-            common.Settings(algorithm_parameters=common.AlgorithmParameters(
-                algorithm_type=common.AlgorithmType.OFF_POLICY_MC_CONTROL
-            )),
-        ],
-        graph_values=common.GraphValues(
-            moving_average_window_size=19,
-            y_min=-200,
-            y_max=0
-        ),
-        grid_view_parameters=common.GridViewParameters(
-            grid_view_type=common.GridViewType.POSITION,
-            show_demo=True,
-            show_trail=True
-        )
+    environment_parameters = EnvironmentParameters(
+        grid=grids.TRACK_1
     )
+    common.set_none_to_default(environment_parameters, default)
+    environment = Environment(environment_parameters)
+    environment.build()
 
-    environment_ = unused_environment_factory.environment_factory(comparison_.environment_parameters)
-
-    for state_ in environment_.states:
-        state_index = environment_.state_index[state_]
+    for state_ in environment.states:
+        state_index = environment.state_index[state_]
         print(f"{state_} \t index={state_index}")
 
     print()
 
-    for action_ in environment_.actions:
-        action_index = environment_.action_index[action_]
+    for action_ in environment.actions:
+        action_index = environment.action_index[action_]
         print(f"{action_} \t index={action_index}")
 
     print()
 
     state_ = state.State(is_terminal=False, position=common.XY(x=4, y=0), velocity=common.XY(x=0, y=1))
     action_ = action.Action(acceleration=common.XY(x=1, y=0))
-    response_ = environment_.from_state_perform_action(state_, action_)
+    response_ = environment.from_state_perform_action(state_, action_)
     print(state_, action_)
     print(response_)
 
     state_ = state.State(is_terminal=False, position=common.XY(x=5, y=4), velocity=common.XY(x=1, y=0))
     action_ = action.Action(acceleration=common.XY(x=0, y=0))
-    response_ = environment_.from_state_perform_action(state_, action_)
+    response_ = environment.from_state_perform_action(state_, action_)
     print(state_, action_)
     print(response_)
 
     state_ = state.State(is_terminal=False, position=common.XY(x=0, y=0), velocity=common.XY(x=0, y=3))
     action_ = action.Action(common.XY(x=0, y=-1))
-    response_ = environment_.from_state_perform_action(state_, action_)
+    response_ = environment.from_state_perform_action(state_, action_)
     print(state_, action_)
     print(response_)
 
--- tests/view_test.py
+++ tests/view_test.py
@@ -1,20 +1,24 @@
 from __future__ import annotations
 
-from mdp import common, scenarios
-from unused import unused_environment_factory
-from mdp.view import view
+from mdp import common
+from mdp.scenarios import scenario_factory
+# from mdp.scenarios.cliff.model.environment import Environment
+# from mdp.view import view
 
 
 def view_test() -> bool:
-    comparison: common.Comparison = scenarios.cliff_episode()
-    # environment_parameters = common.EnvironmentParameters(
-    #     environment_type=common.EnvironmentType.CLIFF,
-    #     actions_list=common.ActionsList.FOUR_MOVES
-    # )
-    cliff = unused_environment_factory.environment_factory(comparison.environment_parameters)
-    my_view = view.View()
-    my_view.build(cliff.grid_world, comparison)
-    my_view.grid_view.display_and_wait()
+    scenario = scenario_factory.scenario_factory(common.ComparisonType.CLIFF_EPISODE)
+    scenario.build()
+    # noinspection PyProtectedMember
+    scenario._view.grid_view.display_and_wait()
+
+    # # noinspection PyProtectedMember
+    # environment: Environment = scenario._model.environment
+    # # noinspection PyProtectedMember
+    # comparison: common.Comparison = scenario._comparison
+    # my_view = view.View()
+    # my_view.build(environment.grid_world, comparison)
+    # my_view.grid_view.display_and_wait()
 
     return True
 
