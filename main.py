e202d31c38c35459474c96680fb7623cf0cd9a93
Robin Carter <68642507+RobinLeeCarter@users.noreply.github.com>
2021-05-15 21:06:46 +0100

    WIP: Believe blackjack is working

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import mdp
 
 def main():
     os_environ_settings.dummy = None    # for pycharm code inspection only
-    mdp.Application(comparison_type=mdp.ComparisonType.BLACKJACK_EVALUATION_Q)
+    mdp.Application(comparison_type=mdp.ComparisonType.BLACKJACK_CONTROL_ES)
 
 
 # if __name__ == '__main__':
