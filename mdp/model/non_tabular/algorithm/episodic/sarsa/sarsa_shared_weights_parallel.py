shared_weights with locks in place but not giving the right result even with just one process so something going wrong in calculation or transfer

mdp/model/non_tabular/algorithm/episodic/sarsa/sarsa_shared_weights_parallel.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
+from typing import Optional
 
-# import numpy as np
+import numpy as np
 
+from mdp.model.non_tabular.agent.non_tabular_agent import NonTabularAgent
 from mdp.model.non_tabular.algorithm.batch_mixin.batch_shared_weights import BatchSharedWeights
 from mdp.model.non_tabular.algorithm.episodic.sarsa.sarsa import Sarsa
 from mdp import common
@@ -10,4 +12,43 @@ from mdp import common
 class SarsaSharedWeightsParallel(Sarsa, BatchSharedWeights,
                                  algorithm_type=common.AlgorithmType.NT_SARSA_SHARED_WEIGHTS_PARALLEL,
                                  algorithm_name="Sarsa Parallel Shared Weights"):
-    pass
+    def __init__(self,
+                 agent: NonTabularAgent,
+                 algorithm_parameters: common.AlgorithmParameters
+                 ):
+        super().__init__(agent, algorithm_parameters)
+        self._previous_feature_vector: Optional[np.ndarray] = None
+
+    def _start_episode(self):
+        ag = self._agent
+        with self._shared_w.lock:
+            ag.choose_action()
+        self._previous_feature_vector = ag.episode[0].feature_vector
+
+    def _do_training_step(self):
+        ag = self._agent
+        ag.take_action()
+        with self._shared_w.lock:
+            ag.choose_action()
+        current_feature_vector = ag.episode[ag.t].feature_vector
+
+        with self._shared_w.lock:
+            # anything involving w
+            previous_q = self.Q.calc_value(self._previous_feature_vector)
+            previous_gradient = self.Q.calc_gradient(self._previous_feature_vector)
+            current_q = self.Q.calc_value(current_feature_vector)
+            target: float = ag.r + self._gamma * current_q
+            delta: float = target - previous_q
+            alpha_delta: float = self._alpha * delta
+
+            if self.Q.has_sparse_feature:
+                # gradient_indices: np.ndarray = self.Q.get_gradient(ag.prev_state, ag.prev_action)
+                self.Q.update_weights_sparse(indices=previous_gradient, delta_w=alpha_delta)
+            else:
+                # gradient_vector: np.ndarray = self.Q.get_gradient(ag.prev_state, ag.prev_action)
+                delta_w: np.ndarray = alpha_delta * previous_gradient
+                # TODO: fix so weights are unchanged
+                self.Q.update_weights(delta_w)
+
+        if not ag.state.is_terminal:
+            self._previous_feature_vector = current_feature_vector

mdp/model/non_tabular/value_function/state_action/linear_state_action_function.py
@@ -40,6 +40,7 @@ class LinearStateActionFunction(StateActionFunction[State, Action],
             self.delta_w: np.ndarray = np.zeros_like(self.w)
             self.original_w: np.ndarray = self.w.copy()
 
+    # combined methods
     def __getitem__(self, state_action: tuple[State, Action]) -> float:
         state, action = state_action
         value: float
@@ -60,13 +61,20 @@ class LinearStateActionFunction(StateActionFunction[State, Action],
         values_array: np.ndarray = self._feature.matrix_product(self._feature_matrix, self.w)
         return values_array
 
+    # @property
+    # def feature(self) -> Optional[BaseFeature[State, Action]]:
+    #     return self._feature
+
     @property
     def feature_matrix(self) -> Optional[np.ndarray]:
         return self._feature_matrix
 
-    # def get_action_values3(self, state: State, actions: list[Action]) -> np.ndarray:
-    #     values_array: np.ndarray = self._feature.get_dot_products(state, actions, self.w)
-    #     return values_array
+    # component methods to separate out interaction with w as much as possible
+    # def get_feature_vector(self, state: State, action: Action) -> np.ndarray:
+    #     return self._feature[state, action]
+    #
+    # def get_feature_matrix(self, state: State, actions: list[Action]) -> np.ndarray:
+    #     return self._feature.get_matrix(state, actions)
 
     def calc_value(self, feature_vector: np.ndarray) -> float:
         value: float = self._feature.dot_product(feature_vector, self.w)