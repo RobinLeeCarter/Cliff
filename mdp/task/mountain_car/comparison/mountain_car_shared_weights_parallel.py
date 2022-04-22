fdd9ee19a71f205995fb0a34503a89ce17fe8b4e
Robin Carter <68642507+RobinLeeCarter@users.noreply.github.com>
2022-04-22 10:38:27 +0100

    Parallel starts well but then becomes unstable. Thought to be previous_q becoming out-of-date due to w changed by other processes. Then change is applied multiple times and this continues creating instability. Also feature_trajectories thought to be poor design and better for agent to have feature optionally and if it does to store the feature_vector in the result_state_action so in the normal trajectory as an extra optional field.

--- mdp/task/mountain_car/comparison/mountain_car_shared_weights_parallel.py
+++ mdp/task/mountain_car/comparison/mountain_car_shared_weights_parallel.py
@@ -16,9 +16,9 @@ class MountainCarSharedWeightsParallel(ComparisonBuilder,
         return Comparison(
             environment_parameters=self._environment_parameters,
             comparison_settings=Settings(
-                training_episodes=1000,
-                episodes_per_batch=1000,
-                episode_print_frequency=100,
+                training_episodes=1600,
+                episodes_per_batch=1600,    # fails if this is large, why? might be previous_q being wrong
+                episode_print_frequency=100,    # store feature for all (s, a) looked up for n-step also?
                 episode_multiprocessing=common.ParallelContextType.FORK_GLOBAL,
                 algorithm_parameters=common.AlgorithmParameters(
                     algorithm_type=common.AlgorithmType.NT_SARSA_SHARED_WEIGHTS_PARALLEL,
